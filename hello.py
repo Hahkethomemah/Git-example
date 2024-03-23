d75de1dfde8df4bb21a3d00b35f5e78be7ca5451
Hahke <d.m.prazmo@gmail.com>
2024-03-23 10:05:56 +0100

    zmienne

--- hello.py
+++ hello.py
@@ -1,2 +1,30 @@
 print("Hello world")
 print("World hello")
+
+my_people_list = [
+  {
+    "name": "Przemex",
+    "age": 19
+  },
+  {
+    "name": "Another Name",
+    "age": 65
+  }
+]
+
+print(my_people_list[0]["name"])
+
+
+my_tuple = ("apple", "banana", "cherry")
+first_item = my_tuple[0]
+
+my_set = {"apple", "banana", "cherry"}
+exists = "banana" in my_set 
+
+a = 10
+b = 20
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
